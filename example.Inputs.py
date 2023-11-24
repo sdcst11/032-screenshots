3d3295e3a7fe2ff47b7e64c97613c64912a11f5e
hungrybeagle <cyang@deltalearns.ca>
2023-11-24 12:26:37 -0800

    Update example.Inputs.py

--- example.Inputs.py
+++ example.Inputs.py
@@ -4,3 +4,5 @@ x = pyautogui.alert("This is an alert box!  Does nothing, but could pause your p
 
 y = pyautogui.prompt("This is a prompt! It allows you to store a value, just like the input command")
 print(f"You entered a {y}")
+
+
