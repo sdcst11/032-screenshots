a85259e3baf94f269aa46f0f293ad80a7db5b93b
hungrybeagle <cyang@deltalearns.ca>
2023-11-24 11:59:44 -0800

    update

---
+++ example.Inputs.py
@@ -0,0 +1,6 @@
+import pyautogui
+
+x = pyautogui.alert("This is an alert box!  Does nothing, but could pause your program")
+
+y = pyautogui.prompt("This is a prompt! It allows you to store a value, just like the input command")
+print(f"You entered a {y}")
---
+++ example.pixels.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+
+x = pyautogui.pixel(100,200)
+print(f"The color of the pixel at (100,200) is {x}")
+print("This tells us the amount of red, green and blue")
+
+if pyautogui.pixelMatchesColor(100,200,x, tolerance=0.9):
+    print("we had a pixel match!")
+
+if pyautogui.pixelMatchesColor(200,200,(52,52,49), tolerance=0.9):
+    print("we had a pixel match!")
+else:
+    print('the second pixel did not match')
+
